a68ed888800c8f21476b8c4ecc547ef7edeb8d4d
OockCuan <st10439216@imconnect.edu.za>
2025-11-20 16:06:21 +0200

    Initial commit
    Set up of basic infrastructure, some models and db context set up

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PROGPOEst10439216.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
